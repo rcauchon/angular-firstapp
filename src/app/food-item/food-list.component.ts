33d509b8edc91293d7de6422a9e5f13ff5e1ac7d
Remi Cauchon <rcauchon@ca.ibm.com>
2019-05-08 10:08:22 -0400

    Add small changes

--- src/app/food-item/food-list.component.ts
+++ src/app/food-item/food-list.component.ts
@@ -21,49 +21,6 @@ export class FoodListComponent implements OnInit {
     this.getFoods();
   }
 
-  reloadFoods(){
-    const newFoodSourceArray = [ {
-      id: 1,
-      name: 'pear',
-      description: 'I am red and crunchy',
-      facts: {
-        servingSize: '1 small apple',
-        calories: '77',
-        fat: '0g',
-        protein: '0g',
-        carbs: '21g'
-      }
-    },
-    {
-      id: 2,
-      name: 'banana',
-      description: 'I am yellow and soft',
-      facts: {
-        servingSize: '1 small banana',
-        calories: '90',
-        fat: '0g',
-        protein: '1g',
-        carbs: '23g'
-      }
-    },
-    {
-      id: 3,
-      name: 'egg',
-      description: 'I am brown and fragile',
-      facts: {
-        servingSize: '1 large egg',
-        calories: '75',
-        fat: '6g',
-        protein: '7g',
-        carbs: '1g'
-      }
-    }
-  ];
-
-    this.fdList = newFoodSourceArray;
-    console.log('New dummy food source array');
-  }
-
   getFoods() {
    // this.fdList = this.foodService.getFoods();
 

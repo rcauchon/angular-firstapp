2e263a66398af19ec20683e6a2422155683e7dc4
Remi Cauchon <rcauchon@ca.ibm.com>
2019-05-15 10:01:38 -0400

    Adding missing dependency

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -9,19 +9,16 @@ import { MyResponse } from '../models/my-response';
 })
 export class LoginService {
 
+  constructor(private http: HttpClient) { }
 
-  constructor(private http:HttpClient) { 
-
-  }
-
-  response = new MyResponse("","","");
+  response = new MyResponse('', '', '');
 
   // Login method
-  login(username:string, password:string){
-    let loginEndPoint = 'http://localhost:3000/loginj';
+  login(username: string, password: string) {
+    const loginEndPoint = 'http://localhost:3000/loginj';
 
     console.log('logging in');
-    console.log(JSON.stringify({"username":username, "password":password}));
+    console.log(JSON.stringify({ username, password}));
 
     let headers = new HttpHeaders();
     headers.append('Content-type', 'application/json');
@@ -29,19 +26,21 @@ export class LoginService {
 
     return this.http.post<MyResponse>(
       loginEndPoint,
-      JSON.stringify({"username": username, "password": password}),
+      JSON.stringify({ username, password}),
       { headers }
     ).pipe(map(response => {
 
-      console.log('Auth success: '+ response.success);
-      console.log('Auth message: '+ response.message);
+      console.log('Auth success: ' + response.success);
+      console.log('Auth message: ' + response.message);
 
-      if (response.success){
+      if (response.success) {
         localStorage.setItem('app_auth_token', response.token);
         console.log('User logged in, token saved');
         console.log(localStorage.getItem('app_auth_token'));
+        return response.token;
+      } else if (!response.success) {
+        return response.message;
       }
-      return response.token;
     }));
 
   }
